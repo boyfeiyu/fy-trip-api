106f28e38abf30aca920b2ef4c896667fecc119c
feiyu <296862383@qq.com>
2022-11-02 15:35:40 +0800

    fixed get home/hotSuggsets data

--- routes/modules/city.js
+++ routes/modules/city.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const cityAll = require('../../data/city/cityall')
-/* GET users listing. */
 router.get('/all', function (req, res, next) {
   res.send(cityAll)
 })
