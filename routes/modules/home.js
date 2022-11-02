ced92e5ce479609f36e08e1050cf46c749c954d8
feiyu <296862383@qq.com>
2022-11-02 15:36:01 +0800

    fixed get home/hotSuggsets data

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const houseList = require('../../data/home/houselist.js')
+const hotSuggestsData = require('../../data/home/hotSuggests')
 /* GET users listing. */
 router.get('/', function (req, res) {
   res.send('home')
@@ -9,6 +10,9 @@ router.get('/', function (req, res) {
 router.get('/houselist', function (req, res, next) {
   const page = parseInt(req.query.page)
   res.send(houseList[page - 1])
+})
+router.get('/hotSuggsets', function (req, res, next) {
+  res.send(hotSuggestsData)
 })
 
 module.exports = router
